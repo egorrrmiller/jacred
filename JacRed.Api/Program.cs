7f93bec43beb7d147f91ddc8063e29914535cd16
egorrrmiller <egorrr.miller@yandex.ru>
2026-02-11 15:39:33 +0300

    change theme

--- JacRed.Api/Program.cs
+++ JacRed.Api/Program.cs
@@ -23,11 +23,41 @@ using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Sinks.SystemConsole.Themes;
 
+var cleanTheme = new AnsiConsoleTheme(new Dictionary<ConsoleThemeStyle, string>
+{
+    // основной текст
+    [ConsoleThemeStyle.Text] = "\x1b[37m",
+
+    // второстепенное
+    [ConsoleThemeStyle.SecondaryText] = "\x1b[90m",
+    [ConsoleThemeStyle.TertiaryText] = "\x1b[90m",
+
+    // данные
+    [ConsoleThemeStyle.String] = "\x1b[32m",     // мягкий зелёный
+    [ConsoleThemeStyle.Number] = "\x1b[35m",     // фиолетовый
+    [ConsoleThemeStyle.Boolean] = "\x1b[36m",    // бирюзовый
+    [ConsoleThemeStyle.Scalar] = "\x1b[32m",
+
+    // уровни
+    [ConsoleThemeStyle.LevelVerbose] = "\x1b[90m",
+    [ConsoleThemeStyle.LevelDebug] = "\x1b[90m",
+
+    [ConsoleThemeStyle.LevelInformation] = "\x1b[36m",  // спокойный cyan
+    [ConsoleThemeStyle.LevelWarning] = "\x1b[33m",      // amber
+    [ConsoleThemeStyle.LevelError] = "\x1b[31m",        // red
+    [ConsoleThemeStyle.LevelFatal] = "\x1b[31;1m",      // яркий красный только для fatal
+
+    // прочее
+    [ConsoleThemeStyle.Name] = "\x1b[37m",
+    [ConsoleThemeStyle.Null] = "\x1b[90m",
+    [ConsoleThemeStyle.Invalid] = "\x1b[33m"
+});
+
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Information()
     .Enrich.FromLogContext()
     .WriteTo.Console(
-        theme: AnsiConsoleTheme.Literate,
+        theme: cleanTheme,
         applyThemeToRedirectedOutput: true)
     .CreateLogger();
 
